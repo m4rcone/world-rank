15cda0aef548b35b78a0c447e6047a4cf939b85f
m4cone <boffmarcone@gmail.com>
2025-02-04 16:49:37 -0300

    feat redirect to neighboring country page

--- app/ui/country/neighboring-countries.tsx
+++ app/ui/country/neighboring-countries.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { fetchCountryByCode } from "../../lib/data";
+import Link from "next/link";
 
 export default async function NeighboringCountries({
   neighbors,
@@ -21,18 +22,17 @@ export default async function NeighboringCountries({
       <div className="flex flex-wrap gap-4">
         {neighboringCountries ? (
           neighboringCountries.flat().map((neighbor) => (
-            <div
-              key={neighbor.cca3}
-              className="flex flex-col items-center justify-center gap-1"
-            >
-              <Image
-                width={96}
-                height={96}
-                src={neighbor.flags?.svg}
-                alt={`Flag of ${neighbor.name?.common}`}
-              />
-              <p className="text-xs">{neighbor.name?.common}</p>
-            </div>
+            <Link key={neighbor.cca3} href={`/country?code=${neighbor.cca3}`}>
+              <div className="flex flex-col items-center justify-center gap-1">
+                <Image
+                  width={96}
+                  height={96}
+                  src={neighbor.flags?.svg}
+                  alt={`Flag of ${neighbor.name?.common}`}
+                />
+                <p className="text-xs">{neighbor.name?.common}</p>
+              </div>
+            </Link>
           ))
         ) : (
           <p className="text-xs">No neighboring countries.</p>
